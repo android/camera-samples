f98da5330fbbefeaa435f72156b2f80643326999
Oscar Wahltinez <owahltinez@google.com>
2019-10-01 20:38:34 -0400

    Refactor Camera2Raw
    - Refactor Camera2Raw sample
    - Add pixel format selection
    - Add JPEG DEPTH pixel format
    
    Change-Id: I1964bebb02d12474b43eb0e57ba7e4b4c81e2235

--- Camera2Formats/app/src/main/java/com/example/android/camera2formats/fragments/PermissionsFragment.kt
+++ Camera2Formats/app/src/main/java/com/example/android/camera2formats/fragments/PermissionsFragment.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.example.cameraxbasic.fragments
+package com.example.android.camera2formats.fragments
 
 import android.Manifest
 import android.content.Context
@@ -23,9 +23,8 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
 import androidx.navigation.Navigation
-import com.android.example.cameraxbasic.R
+import com.example.android.camera2formats.R
 
 private const val PERMISSIONS_REQUEST_CODE = 10
 private val PERMISSIONS_REQUIRED = arrayOf(
@@ -47,7 +46,7 @@ class PermissionsFragment : Fragment() {
         } else {
             // If permissions have already been granted, proceed
             Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                    PermissionsFragmentDirections.actionPermissionsToCamera())
+                    PermissionsFragmentDirections.actionPermissionsToSelector())
         }
     }
 
@@ -55,11 +54,11 @@ class PermissionsFragment : Fragment() {
             requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == PERMISSIONS_REQUEST_CODE) {
-            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Take the user to the success fragment when permission is granted
                 Toast.makeText(context, "Permission request granted", Toast.LENGTH_LONG).show()
                 Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
-                        PermissionsFragmentDirections.actionPermissionsToCamera())
+                        PermissionsFragmentDirections.actionPermissionsToSelector())
             } else {
                 Toast.makeText(context, "Permission request denied", Toast.LENGTH_LONG).show()
             }
