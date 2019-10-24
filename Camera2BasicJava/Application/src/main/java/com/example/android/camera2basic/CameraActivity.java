bd6774285a451b156f61417ea0b478f6b8feaa1c
Jean-Michel Fayard <jmfayard@gmail.com>
2019-10-24 15:41:36 +0200

    Camera2BasicJava: $ ./gradlew migrateToAndroidX
    ```
    
    ## Checking that you use compileSdkVersion 28
    ✔ 🆗 You are using compileSdkVersion 28
    
    ## Migrating classes from support libraries to AndroidX.
    ✔ 🆗 Parsing file androidx-class-mapping.csv
    ✔ 🆗 Modules: [Application]
    ✔ 🆗 Found 18 source files that may need migration
    ✔ 🆗 File androidx-class-mapping.csv parsed correctly
    ✔ 🆗 Starting batch migration...
    Migrating file "CameraActivity.java" … overwriting file…  Done. ✔🆗
    Migrating file "Camera2BasicFragment.java" … overwriting file…  Done. ✔🆗
    Migrating file "build.gradle" …
    ✔ 🆗 2 source files (kt,java,xml) have been migrated (16 didn't need it).
    ✔ 🆗 0 gradle files have been migrated (1 didn't need it).
    
    ## Detecting Gradle properties
    ✔ 🆗 gradle.properties already contains [android.useAndroidX, android.enableJetifier]
    
    ## Your turn: use instead those Androidx libraries
    
    implementation(AndroidX.legacy.supportV4)
    implementation(AndroidX.legacy.supportV13)
    implementation(AndroidX.cardView)
    implementation(AndroidX.appCompat)
    ```

--- Camera2BasicJava/Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
+++ Camera2BasicJava/Application/src/main/java/com/example/android/camera2basic/CameraActivity.java
@@ -17,7 +17,7 @@
 package com.example.android.camera2basic;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class CameraActivity extends AppCompatActivity {
 
