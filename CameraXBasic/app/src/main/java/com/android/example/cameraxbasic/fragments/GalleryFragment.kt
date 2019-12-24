33d9095f9e0dcc92851d6fad8eed46869cc80d9d
ali.azaz <ali.azaz.alam@hotmail.com>
2019-12-24 17:19:35 +0500

    issue #134 crashing resolved

--- CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/GalleryFragment.kt
+++ CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/GalleryFragment.kt
@@ -149,7 +149,11 @@ class GalleryFragment internal constructor() : Fragment() {
 
                             // If all photos have been deleted, return to camera
                             if (mediaList.isEmpty()) {
-                                fragmentManager?.popBackStack()
+//                                fragmentManager?.popBackStack()
+
+                                Navigation.findNavController(requireActivity(), R.id.fragment_container).navigate(
+                                        GalleryFragmentDirections.actionGalleryFragmentToCameraFragment2())
+
                             }
 
                         }
