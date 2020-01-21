5cad868af3397154e0f12c55422b43a22b69215d
Martin Zeitler <syslogic23@gmail.com>
2020-01-21 02:55:43 +0100

    Locale.ROOT removed again.

--- CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/GalleryFragment.kt
+++ CameraXBasic/app/src/main/java/com/android/example/cameraxbasic/fragments/GalleryFragment.kt
@@ -39,7 +39,6 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.android.example.cameraxbasic.utils.showImmersive
 import com.android.example.cameraxbasic.R
-import java.util.Locale
 
 val EXTENSION_WHITELIST = arrayOf("JPG")
 
@@ -70,7 +69,7 @@ class GalleryFragment internal constructor() : Fragment() {
         // Walk through all files in the root directory
         // We reverse the order of the list to present the last photos first
         mediaList = rootDirectory.listFiles { file ->
-            EXTENSION_WHITELIST.contains(file.extension.toUpperCase(Locale.ROOT))
+            EXTENSION_WHITELIST.contains(file.extension.toUpperCase())
         }?.sortedDescending()?.toMutableList() ?: mutableListOf()
     }
 
