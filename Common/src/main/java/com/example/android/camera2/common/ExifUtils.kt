b7090c9a78fd91e989eb319c3605afc3bae41bf8
ankursamarya <ankursamarya@gmail.com>
2020-02-05 12:01:11 +0530

    Correct rotation degree mapping
    Corrected rotation degree mapping to Orientation Exif tag in ExifUtils.kt

--- Common/src/main/java/com/example/android/camera2/common/ExifUtils.kt
+++ Common/src/main/java/com/example/android/camera2/common/ExifUtils.kt
@@ -29,7 +29,6 @@ fun computeExifOrientation(rotationDegrees: Int, mirrored: Boolean) = when {
     rotationDegrees == 0 && mirrored -> ExifInterface.ORIENTATION_FLIP_HORIZONTAL
     rotationDegrees == 180 && !mirrored -> ExifInterface.ORIENTATION_ROTATE_180
     rotationDegrees == 180 && mirrored -> ExifInterface.ORIENTATION_FLIP_VERTICAL
-    rotationDegrees == 270 && mirrored -> ExifInterface.ORIENTATION_TRANSVERSE
     rotationDegrees == 90 && !mirrored -> ExifInterface.ORIENTATION_ROTATE_90
     rotationDegrees == 90 && mirrored -> ExifInterface.ORIENTATION_TRANSPOSE
     rotationDegrees == 270 && mirrored -> ExifInterface.ORIENTATION_ROTATE_270
