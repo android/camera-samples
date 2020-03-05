bf4c6f00688d3f62080ac5642c9e0398e4e31b57
Oscar Wahltinez <oscar@wahltinez.org>
2020-03-05 13:45:01 -0500

    Add config for Github Actions

---
+++ .github/scripts/build_apk.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+# Iterate over each subfolder, which contains an independent project
+# You may edit this if your repo has a different project structure
+for SAMPLE in */ ; do
+    echo "Building APK for $SAMPLE"
+    # Tell Gradle that this is a CI environment and disable parallel compilation
+    bash "${SAMPLE}gradlew" -p "$SAMPLE" -Pci --no-parallel assembleDebug --stacktrace
+done
