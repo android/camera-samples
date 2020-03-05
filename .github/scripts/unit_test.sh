69975e699aaf3558a0ae147bba6a8e2422268212
Oscar Wahltinez <oscar@wahltinez.org>
2020-03-05 14:07:03 -0500

    fix scripts

--- .github/scripts/unit_test.sh
+++ .github/scripts/unit_test.sh
@@ -5,7 +5,7 @@ set -xe
 # You may edit this if your repo has a different project structure
 for SAMPLE in */ ; do
     # Skip folders without a gradlew file
-    [ -f "${SAMPLE}gradlew" ] && continue
+    [ ! -f "${SAMPLE}gradlew" ] && continue
     echo "Running unit tests for $SAMPLE"
     # Tell Gradle that this is a CI environment and disable parallel compilation
     bash "${SAMPLE}gradlew" -p "$SAMPLE" -Pci --no-parallel check --stacktrace
