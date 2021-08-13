f6de74aef8bf4669f889ec8db2da7fde6c1ae046
gfan <gfan@google.com>
2021-08-12 21:51:34 -0700

    Converted yuv.java to Kotlin
    Change-Id: I227989a01e0f244617e3cdccde2a3e3d548e618c

--- CameraUtils/lib/src/main/java/com/example/android/camera/utils/YuvToRgbConverter.kt
+++ CameraUtils/lib/src/main/java/com/example/android/camera/utils/YuvToRgbConverter.kt
@@ -28,43 +28,58 @@ import android.renderscript.Type
 import java.nio.ByteBuffer
 
 /**
- * Helper class used to efficiently convert a [Media.Image] object from
- * [ImageFormat.YUV_420_888] format to an RGB [Bitmap] object.
+ * Helper class used to convert a [Image] object from
+ * [ImageFormat.YUV_420_888] format to an RGB [Bitmap] object, it has equivalent
+ * functionality to https://github
+ * .com/androidx/androidx/blob/androidx-main/camera/camera-core/src/main/java/androidx/camera/core/ImageYuvToRgbConverter.java
+ *
+ * NOTE: This has been tested in a limited number of devices and is not
+ * considered production-ready code. It was created for illustration purposes,
+ * since this is not an efficient camera pipeline due to the multiple copies
+ * required to convert each frame. For example, this
+ * implementation
+ * (https://stackoverflow.com/questions/52726002/camera2-captured-picture-conversion-from-yuv-420-888-to-nv21/52740776#52740776)
+ * might have better performance.
  */
 class YuvToRgbConverter(context: Context) {
     private val rs = RenderScript.create(context)
-    private val scriptYuvToRgb = ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
+    private val scriptYuvToRgb =
+        ScriptIntrinsicYuvToRGB.create(rs, Element.U8_4(rs))
 
-    private var reuseBuffer: ByteBuffer? = null
+    // Do not add getters/setters functions to these private variables
+    // because yuvToRgb() assume they won't be modified elsewhere
+    private var yuvBits: ByteBuffer? = null
     private var bytes: ByteArray = ByteArray(0)
     private var inputAllocation: Allocation? = null
     private var outputAllocation: Allocation? = null
 
-    // for CameraX pass ImageProxy
     @Synchronized
     fun yuvToRgb(image: Image, output: Bitmap) {
-        val converted = Yuv.toBuffer(image, reuseBuffer)
-        reuseBuffer = converted.buffer
+        val yuvBuffer = YuvByteBuffer(image, yuvBits)
+        yuvBits = yuvBuffer.buffer
 
-        if (inputAllocation == null
-            || inputAllocation!!.type.x != image.width
-            || inputAllocation!!.type.y != image.height
-            || inputAllocation!!.type.yuv != converted.type
-            || bytes.size != converted.buffer.capacity()
-        ) {
-            val yuvType: Type.Builder = Type.Builder(rs, Element.U8(rs))
+        if (needCreateAllocations(image, yuvBuffer)) {
+            val yuvType = Type.Builder(rs, Element.U8(rs))
                 .setX(image.width)
                 .setY(image.height)
-                .setYuvFormat(converted.type)
-            inputAllocation = Allocation.createTyped(rs, yuvType.create(), Allocation.USAGE_SCRIPT)
-            bytes = ByteArray(converted.buffer.capacity())
-            val rgbaType: Type.Builder = Type.Builder(rs, Element.RGBA_8888(rs))
+                .setYuvFormat(yuvBuffer.type)
+            inputAllocation = Allocation.createTyped(
+                rs,
+                yuvType.create(),
+                Allocation.USAGE_SCRIPT
+            )
+            bytes = ByteArray(yuvBuffer.buffer.capacity())
+            val rgbaType = Type.Builder(rs, Element.RGBA_8888(rs))
                 .setX(image.width)
                 .setY(image.height)
-            outputAllocation = Allocation.createTyped(rs, rgbaType.create(), Allocation.USAGE_SCRIPT)
+            outputAllocation = Allocation.createTyped(
+                rs,
+                rgbaType.create(),
+                Allocation.USAGE_SCRIPT
+            )
         }
 
-        converted.buffer.get(bytes)
+        yuvBuffer.buffer.get(bytes)
         inputAllocation!!.copyFrom(bytes)
 
         // Convert NV21 or YUV_420_888 format to RGB
@@ -72,5 +87,13 @@ class YuvToRgbConverter(context: Context) {
         scriptYuvToRgb.setInput(inputAllocation)
         scriptYuvToRgb.forEach(outputAllocation)
         outputAllocation!!.copyTo(output)
+    }
+
+    private fun needCreateAllocations(image: Image, yuvBuffer: YuvByteBuffer): Boolean {
+        return (inputAllocation == null ||               // the very 1st call
+            inputAllocation!!.type.x != image.width ||   // image size changed
+            inputAllocation!!.type.y != image.height ||
+            inputAllocation!!.type.yuv != yuvBuffer.type || // image format changed
+            bytes.size == yuvBuffer.buffer.capacity())
     }
 }
